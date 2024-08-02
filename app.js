56a94f7b88d6695464b17096f1a278c45261c182
ronnin47 <tempesttempest66@gmail.com>
2024-08-02 15:55:21 -0300

    login

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ import cors from 'cors';
 import { fileURLToPath } from 'url'; // Importa la función fileURLToPath para convertir __filename a ruta de archivo
 import { dirname, join } from 'path'; // Importa dirname y join para trabajar con rutas de archivos
 
+
+import pkg from 'pg'; 
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
 // Obtiene la ruta del directorio actual
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -14,7 +19,67 @@ app.use(cors()); // Permitir CORS para todas las rutas
 // Servir archivos estáticos desde la carpeta 'dist'
 app.use(express.static(join(__dirname, 'dist')));
 
+
+
+
+
+dotenv.config();
+
+const { Pool } = pkg; 
+
+
+// Configura el límite del tamaño del cuerpo de la solicitud
+app.use(bodyParser.json({ limit: '50mb' }));  // Aumenta el límite a 50 MB para JSON
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Aumenta el límite a 50 MB para URL-encoded
+
+// Servir archivos estáticos desde la carpeta 'dist'
+app.use(express.static(join(__dirname, 'dist')));
+
 const server = http.createServer(app);
+
+
+//LOCAL HOST
+
+// Configuración de la conexión a la base de datos
+const pool = new Pool({
+  user: 'postgres',          // Reemplaza con tu usuario de PostgreSQL
+  host: 'localhost',
+  database: 'newAppZnk', // Reemplaza con el nombre de tu base de datos
+  password: 'hikonometaiseno',   // Reemplaza con tu contraseña de PostgreSQL
+  port: 5432,
+});
+
+
+
+
+
+async function checkDatabaseConnection() {
+  try {
+    // Verifica la conexión a la base de datos
+    await pool.query('SELECT NOW()');
+    console.log('Conexión a la base de datos PostgreSQL exitosa.');
+
+    // Realiza una consulta adicional para verificar que puedes obtener datos
+    const result = await pool.query('SELECT * FROM usuarios LIMIT 1');
+    if (result.rows.length > 0) {
+      console.log('Consulta a la tabla personajes exitosa. Datos obtenidos:');
+      console.log(result.rows); // Imprime una muestra de los datos obtenidos
+    } else {
+      console.log('La tabla usuarios está vacía.');
+    }
+  } catch (err) {
+    console.error('Error al conectar a la base de datos PostgreSQL o al realizar la consulta:', err.message);
+    process.exit(1); // Salir del proceso con un código de error
+  }
+}
+// Llama a la función de verificación de conexión al iniciar el servidor
+checkDatabaseConnection();
+
+app.use(express.json());
+
+
+
+
 const io = new Server(server, {
   cors: {
     origin: '*', // Cambia esto según sea necesario
@@ -32,6 +97,80 @@ io.on('connection', (socket) => {
     console.log('un usuario se desconecto');
   });
 });
+
+
+
+
+
+// ****************INSERTAR USUARIO*************************
+app.post('/insert-usuario', async (req, res) => {
+  const { email, contrasenia } = req.body;
+    console.log("llego la peticion de insert de usuario")
+    console.log(req.body)
+  
+
+  try {
+    const query = `
+      INSERT INTO usuarios (email, contrasenia)
+      VALUES ($1, $2)
+      RETURNING idusuario
+    `;
+
+    const values = [email, contrasenia];
+    const result = await pool.query(query, values);
+
+    const newId = result.rows[0].idusuario;
+    console.log("El Id que viene de la base de datos es: "+newId)
+    res.status(201).json({ message: `Bienvenido ${email}.`, idusuario: newId });
+  } catch (err) {
+
+    if (err.code === '23505') { // Código de error para violación de restricción de unicidad
+      console.error('El email ya existe.');
+      res.json({ message: 'El mail ya se encuentra registrado.' });
+    } else {
+      console.error('Error al insertar el usuario:', err.message);
+      res.status(500).json({ error: 'Error al insertar el usuario.' });
+    }
+
+    
+  }
+});
+
+
+//UPDATE
+/*
+//UPDATE ok!!
+app.put('/update-personaje', async (req, res) => {
+  const { idpersonaje, nombre, dominio, ken, conviccion, imagen } = req.body;
+
+  //console.log("esto viene del req",req.body)
+   // Convierte la imagen de base64 a un buffer
+   const imagenBuffer = Buffer.from(imagen, 'base64');
+  try {
+    // Consulta para actualizar el personaje en la base de datos
+    const result = await pool.query(
+      `UPDATE personajes
+       SET nombre = $1, dominio = $2, ken = $3, conviccion = $4, imagen = $5
+       WHERE idpersonaje = $6`,
+      [nombre, dominio, ken, conviccion, imagenBuffer, idpersonaje]
+    );
+
+    // Verifica si se actualizó algún registro
+    if (result.rowCount > 0) {
+      res.status(200).json({ message: 'Personaje actualizado correctamente' });
+    } else {
+      res.status(404).json({ message: 'Personaje no encontrado' });
+    }
+  } catch (error) {
+    console.error('Error al actualizar el personaje:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+});
+
+*/
+
+
+
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
--- src/components/nava.jsx
+++ src/components/nava.jsx
@@ -1,14 +1,152 @@
 
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
 
 
 export const Nava= ({tituloNav})=> {
 
+
+//visible o no visible
+ const [modalLogin,setModalLogin]=useState(false);
+
+const mostrar=()=>setModalLogin(true);
+const desaparecer=()=>setModalLogin(false);
+
+
+
+
+
+ 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  // Aquí iría la lógica de autenticación
+  console.log('Login submitted');
+  desaparecer();
+};
+
+
+
+//****************NUEVO REGISTRO************
+
+const [modalRegistarme,setModalRegistrarme]=useState(false);
+const mostrarRegistrar=()=>setModalRegistrarme(true);
+const desaparecerRegistrar=()=>setModalRegistrarme(false);
+
+ 
+//estados de lso campos de login
+const[loginEmail,setLoginEmail]=useState("");
+const[loginPassword,setLoginPassword]=useState("")
+
+
+
+
+ // Estados para los campos del formulario de registro
+ const [registroEmail, setRegistroEmail] = useState('');
+ const [registroPassword, setRegistroPassword] = useState('');
+
+const handleSubmitRegistro = (e) => {
+  e.preventDefault();
+  console.log('funciona registarse');
+  
+  
+  
+
+  console.log('Email:', registroEmail);
+  console.log('Password:', registroPassword);
+
+
+
+
+
+//CARGAR NUEVO USUARIO
+
+
+  //PETICION DE INSERT AL SERVIDOR 
+const cargarNuevoUsuario = async () => {
+  const newUsuario = {
+    email: registroEmail,
+    contrasenia: registroPassword,
+  };
+
+
+  
+  try {
+    
+    const response = await axios.post(`http://localhost:4000/insert-usuario`, newUsuario, { 
+    //const response = await axios.post(`https://rankingznk.onrender.com/insert-personaje`, newPersonaje, {   
+    headers: {
+        'Content-Type': 'application/json', // Asegúrate de que el encabezado Content-Type sea application/json
+      },
+    });
+    const { idusuario,message } = response.data;
+    
+    
+
+
+    console.log("El ID de usuario es:", idusuario);
+
+    if (idusuario) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Nuevo registro exitoso',
+        text: message || 'Usuario registrado exitosamente.',
+        footer: `ID de Usuario: ${idusuario}`,
+      });
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: message || 'El mail ya esta registrado.',
+      });
+    }
+
+
+  } catch (error) {
+    console.error('Error al insertar el personaje:', error.message);
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: error.response?.data?.message || 'Error al insertar el usuario/contraseña.',
+    });
+  }
+};
+
+
+cargarNuevoUsuario();
+
+
+
+
+
+
+
+
+
+  
+  
+  setLoginEmail(registroEmail);
+  setLoginPassword(registroPassword);
+  setRegistroEmail("");
+  setRegistroPassword("");
+  desaparecerRegistrar();
+};
+
+
+
   return (
+    <>
     <Navbar expand="lg" bg="dark" variant="dark" className="mb-3">
       <Container fluid>
         <Navbar.Brand href="#">
@@ -28,7 +166,7 @@ export const Nava= ({tituloNav})=> {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1"></Nav.Link>
+            <Nav.Link href="#action1" onClick={mostrar}>Iniciar sesion</Nav.Link>
             <Nav.Link href="#action2"></Nav.Link>          
             <Nav.Link href="#" disabled>
               
@@ -37,5 +175,109 @@ export const Nava= ({tituloNav})=> {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+
+
+
+
+
+
+
+          {/* Modal de Login */}
+          <Modal show={modalLogin} onHide={desaparecer}>
+          <Modal.Header closeButton>
+            <Modal.Title>Inicia sesion ZNK</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="loginEmail">
+                <Form.Label>email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="ingresa tu email"
+                  value={loginEmail}
+                  onChange={(e) => setLoginEmail(e.target.value)}
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group controlId="loginPassword">
+                <Form.Label>contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="ingresa tu contraseña"
+                  value={loginPassword}
+                  onChange={(e) => setLoginPassword(e.target.value)}
+                  required
+                />
+              </Form.Group>
+
+             <div style={{display:"flex", gap:"3px"}}>
+             <Button variant="primary"  className="mt-3" onClick={mostrarRegistrar}>
+                Registrarme
+              </Button>
+              <Button variant="danger"  className="mt-3">
+                Ups! olvide mi contraseña
+              </Button>
+              <Button variant="success" type="submit" className="mt-3">
+                Login
+              </Button>
+             </div>
+              
+            </Form>
+          </Modal.Body>
+          </Modal>
+
+
+
+
+
+
+
+
+
+
+           {/* Modal de Nuevo registro */}
+           <Modal show={modalRegistarme} onHide={desaparecerRegistrar}>
+          <Modal.Header closeButton>
+            <Modal.Title>Registrate</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={handleSubmitRegistro}>
+              <Form.Group controlId="registroEmail">
+                <Form.Label>email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="ingresa tu email"
+                  value={registroEmail}
+                  onChange={(e) => setRegistroEmail(e.target.value)}
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group controlId="registroPassword">
+                <Form.Label>contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="ingresa tu contraseña"
+                  value={registroPassword}
+                  onChange={(e) => setRegistroPassword(e.target.value)}
+                  required
+                />
+              </Form.Group>
+
+           
+              <Button variant="success" type="submit" className="mt-3">
+                registrarme
+              </Button>
+           
+              
+            </Form>
+          </Modal.Body>
+          </Modal>
+
+
+    </>
+    
   );
 }
