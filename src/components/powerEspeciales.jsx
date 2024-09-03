4faa8dbc77a220c28877851d22ba37789fbdc3cc
ronnin47 <tempesttempest66@gmail.com>
2024-09-03 08:51:18 -0300

    separacion2

--- src/components/powerEspeciales.jsx
+++ src/components/powerEspeciales.jsx
@@ -22,8 +22,8 @@ export const PowerEspeciales = ({ poderesFiltrados, nombre, idpersonaje, dominio
         <p style={{ color: "yellow", fontFamily:"cursive" }}>{nombre}</p>
         {técnicasEspeciales.length > 0 ? (
           técnicasEspeciales.map((tecnica, index) => (
-            <div key={index} style={{marginTop:"2em"}}>
-              <p style={{ color: "aliceblue" }}><strong style={{color:"orange"}}>Nombre:</strong> {tecnica.nombre}</p>
+            <div key={index} style={{marginTop:"3.5em"}}>
+              <p style={{ color: "yellow" }}><strong style={{color:"orange"}}>Nombre:</strong> {tecnica.nombre}</p>
               <p style={{ color: "aliceblue" }}><strong style={{color:"orange"}}>Descripción:</strong> {tecnica.descripcion}</p>
               <p style={{ color: "aliceblue" }}><strong style={{color:"orange"}}>Sistema:</strong> {tecnica.sistema}</p>
             </div>
