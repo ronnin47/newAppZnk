dc122d5a8418454613fb7fe1beade9725c0858ff
ronnin47 <tempesttempest66@gmail.com>
2024-12-04 16:34:59 -0300

    nodemailer2

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const pool = new Pool({
 
 
 //**************BASE DICIEMBRE******************
+
 const pool = new Pool({
   user: 'gorda',          
   host: 'dpg-ct685652ng1s738t6fo0-a',
@@ -85,8 +86,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true, // Use `true` for port 465, `false` for all other ports
   auth: {
-    user: process.env.EMAIL_USER,  // Usar la variable de entorno para el usuario
-    pass: process.env.EMAIL_PASS,
+    user: "tempesttempest66@gmail.com",
+    pass: "xghj wmvg pkmr hgzs",
   },
 });
   
