8dda170adb1fbb9deca705efd1cfb2eee8ff3632
ronnin47 <tempesttempest66@gmail.com>
2024-12-09 18:10:29 -0300

    cartis2

--- src/components/miniCard.jsx
+++ src/components/miniCard.jsx
@@ -74,7 +74,7 @@ export const MiniCard = ({ setVivoMuerto, vivoMuerto,setActiveKey,id, nombre,dom
           </div>
       </div>
      <Button variant="outline-warning" onClick={seleccionar} style={{transform:"scale(0.7)" , marginTop:"0px"}} onMouseEnter={(e) => { e.target.style.boxShadow= "0 0 20px 5px rgba(0, 255, 0, 0.8)"  }}
-      onMouseLeave={(e) => { e.target.style.boxShadow = "none" }}>{nombre===""?"desconocido":nombre}</Button>
+      onMouseLeave={(e) => { e.target.style.boxShadow = "none" }}>Seleccionar Pj</Button>
      
     </div>
   )
